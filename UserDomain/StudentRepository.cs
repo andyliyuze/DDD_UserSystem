25c27a2ec69c514c4d1cbd1918eebd492c0c337f
Administrator <Administrator@YOS-01605241440>
2017-08-06 22:56:06 +0800

    第一次

---
+++ UserDomain/StudentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDomain
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public Student Get(Guid userId)
+        {
+
+            Student stu = new Student();
+            return stu;
+        }
+
+
+    }
+}
